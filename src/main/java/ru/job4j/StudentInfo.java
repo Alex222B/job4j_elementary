e0add50f227b6ee6f17c1e494b9439b70dbfc231
Alex22B <x-750f@list.ru>
2021-10-29 01:03:28 +0300

    Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Alexandr Bezsmertnyy");
         System.out.println("17.07.1979");
     }
