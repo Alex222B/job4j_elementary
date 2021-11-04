49934e6551d587bba15a9b5eaf76f0878b9f154d
Alex22B <x-750f@list.ru>
2021-11-04 15:55:05 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+        do {
+             amount += amount * percent - salary;
+             year++;
+        } while (amount > 0);
+        return year;
+    }
+}
