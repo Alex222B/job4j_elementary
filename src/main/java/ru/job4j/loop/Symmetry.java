fed66b527ac6dd8e037ba44bb7c0d3b5fc22dce2
Alex22B <x-750f@list.ru>
2021-11-04 20:10:07 +0300

    симметричная строка

---
+++ src/main/java/ru/job4j/loop/Symmetry.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+public class Symmetry {
+    public static boolean check(int i) {
+        boolean result = true;
+        String str = Integer.toString(i);
+        int j = str.length();
+        for (int ii = 0; ii < str.length(); ii++) {
+            j--;
+            if (str.charAt(ii) != str.charAt(j)) {
+                result = false;
+                break;
+            } else if  (ii == j) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        boolean ffcheck = Symmetry.check(1112111);
+        boolean ff = Symmetry.check(123456321);
+    }
+}
