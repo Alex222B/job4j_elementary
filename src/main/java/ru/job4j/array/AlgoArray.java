ffd792a52e7565262561919d1c50645747a55e0e
Alex222B <x-750f@list.ru>
2021-11-06 01:51:10 +0300

    6.2. Упорядочить массив. [#4441]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -1,11 +1,32 @@
 package ru.job4j.array;
 
+/**
+ * Циклом for проходит от элемента sourceIndex до конца массива,
+ * определяет минимальное значение эл-та и его индекс - destIndex
+ * меняет элементы местами и сдвигает sourceIndex вперед.
+ * Работает while пока sourceIndex не дойдет до предпоследнего эл-та
+ */
 public class AlgoArray {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 3, 2, 1, 4};
-        int temp = array[0]; /* переменная для временного хранения значение ячейки с индексом 0. */
-        array[0] = array[3]; /* записываем в ячейку с индексом 0 значение ячейки с индексом 3. */
-        array[3] = temp; /* записываем в ячейку с индексом 3 значение временной переменной. */
+        int[] array = new int[] {5, 3, 2, 1, 4, 6, 5, 7};
+        int sourceIndex = 0;
+        int destIndex = 0;
+        int currentMinimum = array[0];
+        int temp;
+
+        while (sourceIndex < array.length - 1) {
+            for (int index = sourceIndex; index < array.length; index++) {
+                if (currentMinimum >= array[index]) {
+                    currentMinimum = array[index];
+                    destIndex = index;
+                }
+            }
+            temp = array[sourceIndex];
+            array[sourceIndex] = array[destIndex];
+            array[destIndex] = temp;
+            sourceIndex++;
+            currentMinimum = array[sourceIndex];
+        }
 
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
