dad8e5b16455a0713fd3ec48ef394921913ebd07
Alex <x-750f@list.ru>
2024-10-30 02:03:12 +0300

    4.3. Вызов метода без указания класса [#504945 #461973]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        Calculator.plus(100, 500);
+        plus(100, 500);
         Calculator.plus(4, 2);
         Calculator.plus(3, 5);
     }
