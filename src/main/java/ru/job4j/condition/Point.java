f63bc19dc53888b6d41af630fd46673ef431e4f5
Alex <x-750f@list.ru>
2024-11-01 03:00:24 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,21 +1,24 @@
 package ru.job4j.condition;
 
 public class Point {
+    /**
+     * Вычисляет расстояние между двумя точками на плоскости по их координатам.
+     *
+     * @param x1 координата X первой точки
+     * @param y1 координата Y первой точки
+     * @param x2 координата X второй точки
+     * @param y2 координата Y второй точки
+     * @return расстояние между точками
+     */
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rslX = x2 - x1;
-        double rslY = y2 - y1;
-        rslX = Math.pow(rslX, 2);
-        rslY = Math.pow(rslY, 2);
-        double result = Math.sqrt(rslX + rslY);
-        return result;
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+        return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(1, 0, 4, 0);
-        System.out.println("result (1, 0) to (4, 0) " + result);
-        result = Point.distance(1, 1, 5, 5);
-        System.out.println("result (1, 1) to (5, 5) " + result);
+        System.out.println("Distance (0, 0) to (2, 0) = " + Point.distance(0, 0, 2, 0));
+        System.out.println("Distance (1, 0) to (4, 0) = " + Point.distance(1, 0, 4, 0));
+        System.out.println("Distance (1, 1) to (5, 5) = " + Point.distance(1, 1, 5, 5));
     }
 }
