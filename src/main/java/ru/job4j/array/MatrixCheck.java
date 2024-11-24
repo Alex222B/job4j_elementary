b2250056819cf9465569efa4fd9644742af991c0
Alex <x-750f@list.ru>
2024-11-24 23:28:58 +0300

    Для 6.7.1. корректировка для моностроки

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int cell = 0; cell < board[row].length; cell++) {
-            if (board[row][cell] != 'X') {
+        for (int column = 0; column < board[row].length; column++) {
+            if (board[row][column] != 'X') {
                 result = false;
                 break;
             }
