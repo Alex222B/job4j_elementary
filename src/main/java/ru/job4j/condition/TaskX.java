c3a87573e81211b5d19c34bc7e6c818bedfd960c
Alex <x-750f@list.ru>
2024-12-04 15:00:19 +0300

    Упражнения 1

--- src/main/java/ru/job4j/condition/TaskX.java
+++ src/main/java/ru/job4j/condition/TaskX.java
@@ -1,39 +1,65 @@
 package ru.job4j.condition;
 
 public class TaskX {
-        private static int normalize(int val) {
-            if (val > 9) {
-                val--;
-            } else if (val < 0) {
-                val++;
-            }
-            return val;
-        }
 
-        public static void transform(int number) {
-            int znak = number > 0 ? 1 : -1;
-            number = Math.abs(number);
-            int num1 = number / 100;
-            int num2 = number % 100 / 10;
-            int num3 = number % 10;
-            if (number % 2 == 0) {
-                num1 += num1 % 2 == 0 ? 1 : -1;
-                num2 += num2 % 2 == 0 ? 1 : -1;
-                num3 += num3 % 2 == 0 ? 1 : -1;
-            } else {
-                num1 += num1 % 2 == 0 ? -1 : 1;
-                num2 += num2 % 2 == 0 ? -1 : 1;
-                num3 += num3 % 2 == 0 ? -1 : 1;
+    /**
+     * Метод для подсчета количества замкнутых областей
+     * Проходим циклом по столбцам в каждой строке
+      */
+    public static int countClosedAreas(int[][] map) {
+        int count = 0;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == 0) {
+                    count +=  isClosedArea(map, i, j) ? 1 : 0;
+                }
             }
-            num1 =  normalize(num1);
-            num2 =  normalize(num2);
-            num3 =  normalize(num3);
-
-            System.out.println(znak * (num1 * 100 + num2 * 10 + num3));
         }
+        return count;
+    }
+
+    /**
+     * Вспомогательный метод для проверки замкнутой области
+     * Рекурсивно проверяем на наличие нулей справа, слева и снизу
+     * в текущую ячейку записываем 1
+     */
+    private static boolean isClosedArea(int[][] map, int row, int col) {
+        boolean result = true;
+        map[row][col] = 1;
+        if (col + 1 < map[row].length) {
+            if (map[row][col + 1] == 0) {
+                result &= isClosedArea(map, row, col + 1);
+            }
+        } else {
+            result = false;
+        }
+        if (row + 1 < map.length) {
+            if (map[row + 1][col] == 0) {
+                result &= isClosedArea(map, row + 1, col);
+            }
+        } else {
+            result = false;
+        }
+        if (col - 1 >= 0) {
+            if (map[row][col - 1] == 0) {
+                result &= isClosedArea(map, row, col - 1);
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        TaskX.transform(-109);
+        int[][] map = {
+                {1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 1, 0, 0},
+                {0, 0, 1, 1, 1},
+                {1, 1, 1, 1, 1}
+        };
+
+        System.out.println(TaskX.countClosedAreas(map));
 
     }
 }
